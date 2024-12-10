63e47c0662f1df187b56d33d33a125560a734616
clivendon <84757715+clivendon@users.noreply.github.com>
2024-12-09 22:38:32 -0500

    first commit for v2 of cosmiccoder

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    './index.html',
+    './app.js',
+    './css/*.css',
+    
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./src/*.{html,js}'],
+  },
+  theme: {
+    fontFamily: {
+      sans: ['VT323', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        'universe': "url('/img/universe-1566159.jpg')",
+      },
+      boxShadow: {
+        neon: "0 0 20px theme('colors.purple.200'), 0 0 20px theme('colors.purple.700')"
+      },
+    },
+  },
+  plugins: [
+    require('./plugin'),
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+};
